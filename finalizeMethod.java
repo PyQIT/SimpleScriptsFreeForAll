2b49216f83d83bc95b79725cd4bf90a0cc7107b0
Przemysław Pyk <przempy@gmail.com>
2019-02-13 11:38:39 +0100

    Create finalizeMethod.java

---
+++ finalizeMethod.java
@@ -0,0 +1,28 @@
+class Test1{
+    int x;
+
+    Test1(int i){
+        x = i;
+    }
+
+    protected void finalize(){
+        System.out.println("Object has been deleted with ID: " + x);
+    }
+
+    void generate(int b){
+        Test1 a = new Test1(b);
+    }
+}
+
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Test1 n = new Test1(0);
+
+        for(int i = 0; i < 100000; i++)
+            n.generate(i);
+
+    }
+}
